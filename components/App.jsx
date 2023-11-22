240e350854f2e394a517c474910cffc7757a6109
Taras Tymchii <taras.tymchii@openbet.com>
2023-11-22 16:47:25 +0200

    fixed build errors

--- components/App.jsx
+++ components/App.jsx
@@ -1,27 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ScreenClassProvider, Container } from 'react-grid-system';
-import { addLocales, setLocale, setTranslations } from 'react-i18nify';
-import en from 'date-fns/locale/en-US';
-import nl from 'date-fns/locale/nl';
+
 import GridSystem from './GridSystem.jsx';
-import I18nify from './I18nify.jsx';
 import ResponsiveUtilities from './ResponsiveUtilities.jsx';
 
 function App({ fallbackScreenClass }) {
-  setTranslations({
-    en: {
-      title: 'Awesome app with i18n!',
-      date: 'MMMM do, yyyy',
-    },
-    nl: {
-      title: 'Toffe app met i18n!',
-      date: 'd MMMM yyyy',
-    },
-  });
-  addLocales({ nl, en });
-  setLocale('nl', false);
-
+  useEffect(() => {
+    console.log('App mounted');
+  }, []);
   return (
     <ScreenClassProvider fallbackScreenClass={fallbackScreenClass}>
       <Container>
@@ -41,7 +28,6 @@ function App({ fallbackScreenClass }) {
         <h1>
           React I18nify
         </h1>
-        <I18nify />
       </Container>
     </ScreenClassProvider>
   );
